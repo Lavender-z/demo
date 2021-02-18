ae9f06d4d182f67f78db997e5742b4a96c71f272
Lavender-z <857153784@qq.com>
2021-02-18 23:49:07 +0800

    提交文件

---
+++ 文章图片缩略展示（css+js）/script.js
@@ -0,0 +1,36 @@
+$(function(){
+    $('.carousel-item').eq(0).addClass('active');
+    var total = $('.carousel-item').length;
+    var current = 0;
+    $('#moveRight').on('click', function(){
+      var next=current;
+      current= current+1;
+      setSlide(next, current);
+    });
+    $('#moveLeft').on('click', function(){
+      var prev=current;
+      current = current- 1;
+      setSlide(prev, current);
+    });
+    function setSlide(prev, next){
+      var slide= current;
+      if(next>total-1){
+       slide=0;
+        current=0;
+      }
+      if(next<0){
+        slide=total - 1;
+        current=total - 1;
+      }
+             $('.carousel-item').eq(prev).removeClass('active');
+             $('.carousel-item').eq(slide).addClass('active');
+        setTimeout(function(){
+  
+        },800);
+      
+  
+      
+      console.log('current '+current);
+      console.log('prev '+prev);
+    }
+  });
